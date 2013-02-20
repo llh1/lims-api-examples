db2a27141c469422314af836f6a33ac5f41bf601
Loic <llh1@sanger.ac.uk>
2013-02-20 15:41:05 +0000

    Go through the full dna+rna manual extraction pipeline

--- 2_dna_rna_manual_extraction/dna_rna_manual_extraction.rb
+++ 2_dna_rna_manual_extraction/dna_rna_manual_extraction.rb
@@ -29,14 +29,21 @@ API_ROOT = @options[:url] || "http://localhost:9292"
 HEADERS = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
 API = RestClient::Resource.new(API_ROOT)
 
-TUBE_BARCODE = "XX123456K"
-TUBE_ROLE = "tube_to_be_extracted"
-TUBE_ALIQUOT_TYPE_RNAP = "RNA+P"
-SPIN_ALIQUOT_TYPE = "DNA"
+NAP_TUBE_BARCODE = "XX123456K"
 
-# ================
+ALIQUOT_TYPE_RNAP = "RNA+P"
+ALIQUOT_TYPE_DNA = "DNA"
+ALIQUOT_TYPE_RNA = "RNA"
+
+ROLE_TUBE_TO_BE_EXTRACTED = "tube_to_be_extracted"
+ROLE_BY_PRODUCT_TUBE = "by_product_tube"
+ROLE_BINDING_SPIN_COLUMN = "binding_spin_column"
+ROLE_ELUTION_SPIN_COLUMN = "elution_spin_column"
+ROLE_EXTRACTED_TUBE = "extracted_tube"
+
+
 # Helper functions
-# ================
+
 def post(url, parameters)
   response = API[url].post(parameters.to_json, HEADERS)
   dump_request("post", url, parameters, response)
@@ -65,50 +72,250 @@ def dump_request(method, url, parameters, response)
   end
 end
 
-# ============================
-# 1 - Find the tube by barcode
-# ============================
+# =======================================
+# Search the source tube in the order and
+# assign it a batch
+# =======================================
+
+# Find the tube by barcode
 parameters = {:search => {:description => "search for barcoded tube",
                           :model => "tube",
                           :criteria => {:label => {:position => "barcode",
                                                    :type => "sanger-barcode",
-                                                   :value => TUBE_BARCODE}}}}
+                                                   :value => NAP_TUBE_BARCODE}}}}
 search_response = post("searches", parameters)
 result_url = search_response["search"]["actions"]["first"]
 result_response = get(result_url) 
-tube = result_response["tubes"].first["tube"]
+source_tube_uuid = result_response["tubes"].first["tube"]["uuid"]
 
-# ========================================
-# 2 - Find the order by tube uuid and role
-# ========================================
+# Find the order by tube uuid and role
 parameters = {:search => {:description => "search for order",
                           :model => "order",
-                          :criteria => {:item => {:uuid => tube["uuid"],
-                                                  :role => TUBE_ROLE}}}}
+                          :criteria => {:item => {:uuid => source_tube_uuid,
+                                                  :role => ROLE_TUBE_TO_BE_EXTRACTED}}}}
 search_response = post("searches", parameters)
 result_url = search_response["search"]["actions"]["first"]
 result_response = get(result_url)
 order = result_response["orders"].first["order"]
+order_uuid = order["uuid"]
 
-# =====================================================
-# 3 - Check that no batch has been assigned to the tube
-# =====================================================
-no_batch = order["items"][TUBE_ROLE].select do |item|
-  item["uuid"] == tube["uuid"]
+# Check that no batch has been assigned to the tube
+no_batch = order["items"][ROLE_TUBE_TO_BE_EXTRACTED].select do |item|
+  item["uuid"] == source_tube_uuid 
 end.first["batch"].nil?
 
-exit unless no_batch
+abort("Error: A batch is already assigned to the source tube") unless no_batch
 
-# ======================
-# 4 - Create a new batch
-# ======================
+# Create a new batch
 parameters = {:batch => {}}
 response = post("batches", parameters)
-batch = response["batch"]
+batch_uuid = response["batch"]["uuid"]
 
-# =======================================================
-# 5 - Assign the batch uuid to the tube in the order item
-# =======================================================
-parameters = {:items => {TUBE_ROLE => {tube["uuid"] => {:batch_uuid => batch["uuid"]}}}}
-put(order["uuid"], parameters)
+# Assign the batch uuid to the tube in the order item
+parameters = {:items => {ROLE_TUBE_TO_BE_EXTRACTED => {source_tube_uuid => {:batch_uuid => batch_uuid}}}}
+put(order_uuid, parameters)
+
+
+# =========================
+# Build and start the order
+# =========================
+
+# Change the status of the order to pending
+parameters = {:event => :build}
+put(order_uuid, parameters)
+
+# Change the status of the order to in_progress
+parameters = {:event => :start}
+put(order_uuid, parameters)
+
+
+# =====================================
+# Transfer the DNA from the source tube
+# into a spin column
+# =====================================
+
+# Create a new spin column
+parameters = {:spin_column => {}}
+response = post("spin_columns", parameters)
+dna_spin_uuid = response["spin_column"]["uuid"]
+
+# Add the new spin column in the order and start it
+parameters = {:items => {ROLE_BINDING_SPIN_COLUMN => {dna_spin_uuid => {:event => :start}}}}
+put(order_uuid, parameters)
+
+# Transfer from source tube into the spin column
+parameters = {:transfer_tubes_to_tubes => {:transfers => [{
+  :source_uuid => source_tube_uuid, 
+  :target_uuid => dna_spin_uuid, 
+  :fraction => 0.5, 
+  :aliquot_type => ALIQUOT_TYPE_DNA
+}]}}
+post("actions/transfer_tubes_to_tubes", parameters)
+
+# Change the status of the spin column to done
+parameters = {:items => {ROLE_BINDING_SPIN_COLUMN => {dna_spin_uuid => {:event => :complete}}}}
+put(order_uuid, parameters)
+
+
+# ==============================================
+# Use the spin column in a new role elution
+# and transfer the content to an extracted tube
+# ==============================================
+
+# Add the spin column in the order under the role elution and start it
+parameters = {:items => {ROLE_ELUTION_SPIN_COLUMN => {dna_spin_uuid => {:event => :start}}}}
+put(order_uuid, parameters)
+
+# Change the status of the spin column to done
+parameters = {:items => {ROLE_ELUTION_SPIN_COLUMN => {dna_spin_uuid => {:event => :complete}}}}
+put(order_uuid, parameters)
+
+# Create a new tube
+parameters = {:tube => {}}
+response = post("tubes", parameters)
+dna_tube_uuid = response["tube"]["uuid"]
+
+# Add the tube in the order and start it
+parameters = {:items => {ROLE_EXTRACTED_TUBE => {dna_tube_uuid => {:event => :start}}}}
+put(order_uuid, parameters)
+
+# Transfer the spin column to the extracted_tube
+parameters = {:transfer_tubes_to_tubes => {:transfers => [{
+  :source_uuid => dna_spin_uuid, 
+  :target_uuid => dna_tube_uuid, 
+  :amount => 5, 
+  :aliquot_type => ALIQUOT_TYPE_DNA
+}]}}
+post("actions/transfer_tubes_to_tubes", parameters)
+
+# Change the status of the dna tube to done
+parameters = {:items => {ROLE_EXTRACTED_TUBE => {dna_tube_uuid => {:event => :complete}}}}
+put(order_uuid, parameters)
+
+
+# =================================
+# Transfer from source tube to a 
+# by product tube containing RNA+P
+# =================================
+
+# Create a new tube
+parameters = {:tube => {}}
+response = post("tubes", parameters)
+rnap_tube_uuid = response["tube"]["uuid"]
+
+# Add the new tube in the order and start it
+parameters = {:items => {ROLE_BY_PRODUCT_TUBE => {rnap_tube_uuid => {:event => :start}}}}
+put(order_uuid, parameters)
+
+# Transfer the source tube to rna+p tube
+parameters = {:transfer_tubes_to_tubes => {:transfers => [{
+  :source_uuid => source_tube_uuid, 
+  :target_uuid => rnap_tube_uuid, 
+  :fraction => 1, 
+  :aliquot_type => ALIQUOT_TYPE_RNAP
+}]}}
+post("actions/transfer_tubes_to_tubes", parameters)
+
+# Change the status of the by product tube to done
+parameters = {:items => {ROLE_BY_PRODUCT_TUBE => {rnap_tube_uuid => {:event => :complete}}}}
+put(order_uuid, parameters)
+
+
+# =====================================
+# Transfer from by product tube to tube 
+# to be extracted
+# =====================================
+
+# Create a new tube
+parameters = {:tube => {}}
+response = post("tubes", parameters)
+rnap_tube2_uuid = response["tube"]["uuid"]
+
+# Add the new tube in the order and start it
+parameters = {:items => {ROLE_TUBE_TO_BE_EXTRACTED => {rnap_tube2_uuid => {:event => :start}}}}
+put(order_uuid, parameters)
+
+# Transfer the by product tube to tube to be extracted
+parameters = {:transfer_tubes_to_tubes => {:transfers => [{
+  :source_uuid => rnap_tube_uuid, 
+  :target_uuid => rnap_tube2_uuid, 
+  :amount => 5
+}]}}
+post("actions/transfer_tubes_to_tubes", parameters)
+
+# Change the status of the by product tube to done
+parameters = {:items => {ROLE_TUBE_TO_BE_EXTRACTED => {rnap_tube2_uuid => {:event => :complete}}}}
+put(order_uuid, parameters)
+
+
+# =============================================
+# Transfer the content of tube to be extracted
+# into a spin column
+# =============================================
+
+# Create a new spin column
+parameters = {:spin_column => {}}
+response = post("spin_columns", parameters)
+rna_spin_uuid = response["spin_column"]["uuid"]
+
+# Add the new spin column in the order and start it
+parameters = {:items => {ROLE_BINDING_SPIN_COLUMN => {rna_spin_uuid => {:event => :start}}}}
+put(order_uuid, parameters)
+
+# Transfer from tube to be extracted into the spin column
+parameters = {:transfer_tubes_to_tubes => {:transfers => [{
+  :source_uuid => rnap_tube2_uuid, 
+  :target_uuid => rna_spin_uuid, 
+  :amount => 1, 
+  :aliquot_type => ALIQUOT_TYPE_RNA
+}]}}
+post("actions/transfer_tubes_to_tubes", parameters)
+
+# Change the status of the spin column to done
+parameters = {:items => {ROLE_BINDING_SPIN_COLUMN => {rna_spin_uuid => {:event => :complete}}}}
+put(order_uuid, parameters)
+
+
+# ==============================================
+# Use the spin column in a new role elution
+# and transfer the content to an extracted tube
+# ==============================================
+
+# Add the spin column in the order under the role elution and start it
+parameters = {:items => {ROLE_ELUTION_SPIN_COLUMN => {rna_spin_uuid => {:event => :start}}}}
+put(order_uuid, parameters)
+
+# Change the status of the spin column to done
+parameters = {:items => {ROLE_ELUTION_SPIN_COLUMN => {rna_spin_uuid => {:event => :complete}}}}
+put(order_uuid, parameters)
+
+# Create a new tube
+parameters = {:tube => {}}
+response = post("tubes", parameters)
+rna_tube_uuid = response["tube"]["uuid"]
+
+# Add the tube in the order and start it
+parameters = {:items => {ROLE_EXTRACTED_TUBE => {rna_tube_uuid => {:event => :start}}}}
+put(order_uuid, parameters)
+
+# Transfer the spin column to the extracted_tube
+parameters = {:transfer_tubes_to_tubes => {:transfers => [{
+  :source_uuid => rna_spin_uuid, 
+  :target_uuid => rna_tube_uuid, 
+  :amount => 1, 
+  :aliquot_type => ALIQUOT_TYPE_RNA
+}]}}
+post("actions/transfer_tubes_to_tubes", parameters)
+
+# Change the status of the rna tube to done
+parameters = {:items => {ROLE_EXTRACTED_TUBE => {rna_tube_uuid => {:event => :complete}}}}
+put(order_uuid, parameters)
+
+
+# ============================================
+# Change the status of the order to completed
+# ============================================
+
+parameters = {:event => :complete}
+put(order_uuid, parameters)
 
